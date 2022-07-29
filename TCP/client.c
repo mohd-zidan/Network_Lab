9ff3c43ee2587597e39c7c63cf21ec419f062861
mohd-zidan <72930957+mohd-zidan@users.noreply.github.com>
2022-07-29 22:10:34 +0530

    PFA

---
+++ TCP/client.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main(int argc, char *argv[])
+{
+struct sockaddr_in server;
+int sd;
+char buffer[200];
+if((sd=socket(AF_INET,SOCK_STREAM,0))<0)
+{
+perror("Socket failed:");
+exit(1);
+}
+bzero(&server,sizeof(server));
+server.sin_family=AF_INET;
+server.sin_port=htons(atoi(argv[2]));
+inet_pton(AF_INET,argv[1],&server.sin_addr);
+if(connect(sd,(struct sockaddr *)&server,sizeof(server))<0)
+{
+perror("Connection failed:");
+exit(1);
+}
+printf("Server is Connected:\n");
+printf("Enter the string:\n");
+fgets(buffer,sizeof(buffer),stdin);
+buffer[strlen(buffer)-1]='\0';
+write(sd,buffer,sizeof(buffer));
+read(sd,buffer,sizeof(buffer));
+printf("The reversed string is: %s\n",buffer);
+close(sd);
+}
---
+++ UDP/client.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+int i,j,n;
+int sock_fd;
+struct sockaddr_in servaddr;
+int matrix_1[10][10],matrix_2[10][10],matrix_product[10][10];
+int size[2][2];
+int num_rows_1,num_cols_1,num_rows_2,num_cols_2;
+if(argc!=3)
+{
+fprintf(stderr,"Usage:./client IP address_of_server port\n");
+exit(1);
+}
+printf("Enter the number of rows for the first matrix:\n");
+scanf("%d",&num_rows_1);
+printf("Enter the number of columns of first matrix:\n");
+scanf("%d",&num_cols_1);
+printf("Enter the value row by row one on each line:\n");
+
+for(i=0;i<num_rows_1;i++)
+for(j=0;j<num_cols_1;j++)
+{
+scanf("%d",&matrix_1[i][j]);
+}
+size[0][0]=num_rows_1;
+size[0][1]=num_cols_1;
+printf("Enter the number of rows of second matrix:\n");
+scanf("%d",&num_rows_2);
+printf("Enter the number of columns of second matrix:\n");
+scanf("%d",&num_cols_2);
+if(num_cols_1 != num_rows_2)
+{
+printf("MATRICES CANNOT BE MULTIPLE\n");
+exit(1);
+}
+printf("Enter the values for row by row on each line: \n");
+for(i=0;i<num_rows_2;i++)
+for(j=0;j<num_cols_2;j++)
+{
+scanf("%d",&matrix_2[i][j]);
+}
+size[1][0]=num_rows_2;
+size[1][1]=num_cols_2;
+if((sock_fd=socket(AF_INET,SOCK_DGRAM,0))<0)
+{
+printf("Cannot create socket!\n");
+exit(1);
+}
+bzero((char*)&servaddr,sizeof(servaddr));
+servaddr.sin_family=AF_INET;
+servaddr.sin_port=htons(atoi(argv[2]));
+inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+//SENDING MATRIX 1 & 2 WITH SIZES
+n=sendto(sock_fd,size,sizeof(size),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+if(n<0)
+{
+perror("error in matrix 1 sending");
+exit(1);
+}
+// SENDING MATRIX 1
+n=sendto(sock_fd,matrix_1,sizeof(matrix_1),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+if(n<0)
+{
+perror("error in matrix 1 sending");
+exit(1);
+}
+//SENDING MATRIX 2
+n=sendto(sock_fd,matrix_2,sizeof(matrix_2),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+if(n<0)
+{
+perror("error in matrix 2 sending");
+
+exit(1);
+}
+if((n=recvfrom(sock_fd,matrix_product,sizeof(matrix_product),0,NULL,NULL))==-1)
+{
+perror("read error from server");
+exit(1);
+}
+printf("\n\n THE PRODUCT OF THE MATRICES IS \n\n\n");
+for(i=0;i<num_rows_1;i++)
+{
+for(j=0;j<num_cols_2;j++)
+{
+printf("%d \t",matrix_product[i][j]);
+}
+printf("\n");}
+close(sock_fd);
+}
